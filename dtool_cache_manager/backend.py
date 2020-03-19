192ea3f6fbb603ad6921df8807508966b62b7f12
Tjelvar Olsson <olssont@nbi.ac.uk>
2020-03-19 12:45:54 +0000

    Implement sqlite backend and manager as object

--- dtool_cache_manager/backend.py
+++ dtool_cache_manager/backend.py
@@ -1,2 +1,71 @@
 """Module for dealing with dtool cache manager persistance."""
+import datetime
 import sqlite3
+
+"""
+>>> db = sqlite3.connect(':memory:', detect_types=sqlite3.PARSE_DECLTYPES)
+>>> c = db.cursor()
+>>> c.execute('create table foo (bar integer, baz timestamp)')
+<sqlite3.Cursor object at 0x40fc50>
+>>> c.execute('insert into foo values(?, ?)', (23, datetime.datetime.now()))
+<sqlite3.Cursor object at 0x40fc50>
+>>> c.execute('select * from foo')
+<sqlite3.Cursor object at 0x40fc50>
+>>> c.fetchall()
+[(23, datetime.datetime(2009, 12, 1, 19, 31, 1, 40113))]
+"""
+
+
+class BackendManager(object):
+    """Class for managing cache manager backend."""
+
+    def __init__(self, fpath):
+        self.conn = sqlite3.connect(
+            fpath,
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        self.cur = self.conn.cursor()
+        self.cur.execute('''
+            CREATE TABLE IF NOT EXISTS dtcache
+            (
+              dataset_uuid     TEXT NOT NULL,
+              item_id          TEXT NOT NULL,
+              size_in_bytes     INTEGER  NOT NULL,
+              last_access_time TIMESTAMP NOT NULL );
+        ''')
+
+
+    def add_new_entry(self, dataset_uuid, item_id, size_in_bytes):
+        """Add a new entry to the backend."""
+        now = datetime.datetime.utcnow()
+        entry = (dataset_uuid, item_id, size_in_bytes, now)
+        self.cur.execute(
+            '''INSERT INTO dtcache VALUES (?,?,?,?);''',
+            entry
+        )
+
+    def total_size_in_bytes(self):
+        """Return the total size in bytes."""
+        self.cur.execute('''
+            SELECT SUM(size_in_bytes) FROM dtcache;
+        ''')
+        total = self.cur.fetchone()[0]
+        if total is None:
+            total = 0
+        return total
+
+    def total_number_of_entries(self):
+        """Return the number of entires in the backend."""
+        self.cur.execute('''
+            SELECT COUNT(*) FROM dtcache;
+        ''')
+        count = self.cur.fetchone()[0]
+        return count
+
+    def delete_entry(self, dataset_uuid, item_id):
+        """Delete an entry from the backend."""
+        self.cur.execute('''
+            DELETE FROM dtcache WHERE (dataset_uuid=? AND item_id=?)
+        ''',
+            (dataset_uuid, item_id)
+        )
--- tests/test_backend.py
+++ tests/test_backend.py
@@ -2,6 +2,8 @@
 import os
 import uuid
 
+import dtoolcore
+
 from . import tmp_dir_fixture  # NOQA
 
 
@@ -11,10 +13,26 @@ def test_import():
     )
 
 
+def test_create_database(tmp_dir_fixture):
+    from dtool_cache_manager.backend import BackendManager
+
+    db_fpath = os.path.join(tmp_dir_fixture, "tmp.db")
+    assert not os.path.isfile(db_fpath)
+
+    bm = BackendManager(":memory:")
+    stupid_fpath = os.path.join(tmp_dir_fixture, ":memory:")
+    assert not os.path.isfile(stupid_fpath)
+
+    bm = BackendManager(db_fpath)
+    assert os.path.isfile(db_fpath)
+
+    bm2 = BackendManager(db_fpath)
+
+
 def test_functional(tmp_dir_fixture):  # NOQA
     from dtool_cache_manager.backend import BackendManager
     bm = BackendManager(":memory:")
-    
+
 
     # Create info for a dummy item in a dataset.
     dataset_uuid = str(uuid.uuid4())
@@ -25,10 +43,10 @@ def test_functional(tmp_dir_fixture):  # NOQA
     bm.add_new_entry(dataset_uuid=dataset_uuid, item_id=item_id, size_in_bytes=size)
 
     assert bm.total_size_in_bytes() == 13
-    assert bm.total_number_of_entires() == 1
+    assert bm.total_number_of_entries() == 1
 
     bm.delete_entry(dataset_uuid=dataset_uuid, item_id=item_id)
 
-    
+
     assert bm.total_size_in_bytes() == 0
-    assert bm.total_number_of_entires() == 0
+    assert bm.total_number_of_entries() == 0
